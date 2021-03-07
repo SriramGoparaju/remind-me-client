4569db61be4a3f9183b5106e1022c280645870bc
Radd <sriramgoparaju99@gmail.com>
2021-03-07 11:09:05 +0530

    changed the uri to deployed backend

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,7 +4,7 @@ import { ApolloProvider, InMemoryCache, ApolloClient , createHttpLink} from "@ap
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+	uri: "https://stormy-sands-09027.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
